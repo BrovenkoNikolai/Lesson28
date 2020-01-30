fc1f06befc05f79696163291869855e847b50fea
niko-brazo niko-brazo <brovenko_nikolai@mail.ru>
2020-01-30 17:54:06 +0300

    Добавьте файлы проекта.

---
+++ GarbageCollection_Urok28/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GarbageCollection_Urok28
+{
+    class MyClass : IDisposable
+    {
+        public MyClass() { }
+
+        ~MyClass() { } // диструктор должен быть без аргумента
+
+        public void Dispose()
+        {
+            GC.Collect();
+        }
+    }
+}
---
+++ GarbageCollection_Urok28/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GarbageCollection_Urok28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GC.GetTotalMemory(false));
+            // GC.Collect(); // GC - Сборщик мусора. (0) проверка первого поколения, (1) второго поколения и (2) третьего
+            for (int i = 0; i < 10000; i++)
+            {
+                var obj = (object)i;
+                int j = (int)obj;
+            }
+
+            using (var c = new MyClass())
+            {
+
+            }
+
+            Console.WriteLine(GC.GetTotalMemory(false));
+
+            GC.Collect();
+
+            Console.WriteLine(GC.GetTotalMemory(false));
+
+            Console.ReadLine();
+        }
+    }
+}
